814b9ee089d12f4843de7a7df9e969e4ae318991
aniketsingh-work <worksontheway11@gmail.com>
2026-03-09 00:44:42 +0530

    Half way to go

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class ASLModel(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(ASLModel, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        return self.network(x)
